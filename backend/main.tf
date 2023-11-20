393dbe08f12e32d77b59204bd4828a9c170906c7
Igor Zalutski <izalutski@gmail.com>
2023-11-20 15:18:23 +0000

    Add backend

---
+++ backend/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("gcp_key.json")
+
+  project = "digger-landing"
+  region  = "europe-west2"
+  zone    = "europe-west2-a"
+}
+
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
